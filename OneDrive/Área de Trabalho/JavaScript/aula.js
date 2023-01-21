cfd5987a2af230956b9365416cff3d2b45a71ad7
Caio de Souza Silva <caioss20.cs@gmail.com>
2023-01-20 22:00:22 -0300

    caio

---
+++ OneDrive/Área de Trabalho/JavaScript/aula.js
@@ -0,0 +1,113 @@
+// // // // let idade = 5;
+
+// // // // console.log(idade);
+
+
+// // // let valorIngresso = 20;
+// // // valorIngresso = 30;
+// // // console.log(valorIngresso);
+// // // DESTA FORMA, O VALOR DO CONSOLE SERÁ 30
+
+
+
+// // // const valorIngresso = 20;
+// // // valorIngresso = 30;
+// // // console.log(valorIngresso);
+// // // DESTA FORMA, DARÁ ERRO, CONST NÃO SE MODIFICA
+
+
+
+// // // TIPOS PRIMITIVOS
+
+// // // let nome = 'Caio'; - TIPO STRING
+// // // let idade = 25; - TIPO NUMBER
+// // //let estaAprovado = true; - TIPO BOOLEAN
+// // // let sobrenome ; - UNDEFINED
+// // // let corSelecionada = null - VAZIO/NULL USADO PARA REDEFINIR VALORES
+
+
+
+// // // OBJETOS - Basicamente objetos são um conjunto de dados armazenados em um lugar só.
+
+// // // let pessoa = {
+// // // nome: 'Caio',
+// // // idade: 22,
+// // // altura: 1.82,
+// // // sexo: 'masculino'
+
+
+// // // };
+
+
+
+// // // ARRAYS - Basicamente é um conjunto de dados que podem ser manipulados por indices
+
+// // // let familia = [22,61,32,57];
+
+// // // console.log(familia[3]);
+
+
+
+
+
+// // FUNCTIONS - Controla todo fluxo de execução. Toda entrada e saída de dados são ultilizadas funções.
+
+// // let corSite = 'azul';
+// // function resetaCor () {
+// //     corSite = "";
+// //     };
+// //     console.log(corSite);
+// //     resetaCor();
+// //     console.log(corSite);
+
+
+
+
+
+// // TIPOS DE FUNÇÕES
+// // Função que realiza uma tarefa e não devolve nada
+
+// function dizerNome(){
+// console.log('Caio')
+
+// };
+// dizerNome();
+
+
+
+// faz um calculo oou operação e retorna algo
+// function multiplicaPorDois (valor){
+// return valor* 2;
+
+// };
+
+// console.log(multiplicaPorDois(500));
+
+
+// Operadores aritiméticos - +   -  *  /  **
+
+
+// Operadores lógicos
+
+
+// Operadores de atribuição
+
+
+// Operadores de comparação
+
+
+// Operadores Bitwise
+
+
+// let varA = 'a';
+// let varB = 'b';
+// let varC = 'c';
+
+// varA = varB;
+// varB = varC;
+// varC = varA;
+
+
+// console.log(varA , varB, varC);
+
+
