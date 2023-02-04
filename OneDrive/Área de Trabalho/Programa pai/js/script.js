625e4aba45882e87f046895d87c20428099bbfad
Caio de Souza Silva <89036100+CaioSouzaDevv@users.noreply.github.com>
2023-02-04 15:31:18 -0300

    gerador de números aleátórios sendo os números mais sorteados da mega sena

---
+++ OneDrive/Área de Trabalho/Programa pai/js/script.js
@@ -0,0 +1,14 @@
+// const button = document.getElementById('generate');
+
+// button.addEventListener('click', function() {
+//     const min = [01, 02, 4, 5, 7, 9, 12, 14, 17, 18, 19, 20, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 38, 40, 
+//     41, 43, 44, 45, 46, 47, 48, 49, 51, 57, 55, 56, 57, 58];
+    
+
+
+//     const numero = Math.floor(Math.random() * min.length);
+
+//     document.querySelector('#result > span').textContent = numero;
+
+// });
+
